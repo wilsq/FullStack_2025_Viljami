b08727a2b204f030af20cb6cc538c6c123bb98dc
Viljami V. <viljami.j.viinikainen@gmail.com>
2025-02-12 15:29:16 +0200

    server commit

---
+++ WS02a/server.js
@@ -0,0 +1,13 @@
+const http = require("http");
+
+http
+  .createServer(function (request, response) {
+    response.writeHead(200, {
+      "Content-Type": "text/plain",
+    });
+    response.write("Hello World!");
+    response.end();
+  })
+  .listen(8081);
+
+console.log("Server started on http://localhost:8081");
